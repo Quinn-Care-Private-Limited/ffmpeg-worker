04ba2292c5c8bd0b68a7e20043ff07c9164baab5
Shubhankar Das <das.shubhankar97@gmail.com>
2024-01-03 22:40:11 +0530

    updated types

--- ffmpeg-sdk/lib/types.ts
+++ ffmpeg-sdk/lib/types.ts
@@ -43,6 +43,18 @@ export interface IAWSCredentials {
   region: string;
 }
 
-export interface IGCPCredentials {}
+export interface IGCPCredentials {
+  type: string;
+  project_id: string;
+  private_key_id: string;
+  private_key: string;
+  client_email: string;
+  client_id: string;
+  auth_uri: string;
+  token_uri: string;
+  auth_provider_x509_cert_url: string;
+  client_x509_cert_url: string;
+  universe_domain: string;
+}
 
 export type ICloudStorageCredentials = IAWSCredentials | IGCPCredentials;
--- src/types.ts
+++ src/types.ts
@@ -10,7 +10,19 @@ export interface IAWSCredentials {
   region: string;
 }
 
-export type IGCPCredentials = any;
+export interface IGCPCredentials {
+  type: string;
+  project_id: string;
+  private_key_id: string;
+  private_key: string;
+  client_email: string;
+  client_id: string;
+  auth_uri: string;
+  token_uri: string;
+  auth_provider_x509_cert_url: string;
+  client_x509_cert_url: string;
+  universe_domain: string;
+}
 
 export type ICloudStorageCredentials = IAWSCredentials | IGCPCredentials;
 export interface IWebhookResponse {
