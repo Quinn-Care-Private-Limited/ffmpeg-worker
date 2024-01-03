ce7166307093665a77a59afdaa644cfd4297c932
Shubhankar Das <das.shubhankar97@gmail.com>
2024-01-04 00:03:36 +0530

    fixed types

--- src/types.ts
+++ src/types.ts
@@ -4,25 +4,8 @@ export enum WebhookType {
   STORAGE_DOWNLOAD = "STORAGE_DOWNLOAD",
 }
 
-export interface IAWSCredentials {
-  accessKeyId: string;
-  secretAccessKey: string;
-  region: string;
-}
-
-export interface IGCPCredentials {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_key: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-  universe_domain: string;
-}
+export type IAWSCredentials = any;
+export type IGCPCredentials = any;
 
 export type ICloudStorageCredentials = IAWSCredentials | IGCPCredentials;
 export interface IWebhookResponse {
