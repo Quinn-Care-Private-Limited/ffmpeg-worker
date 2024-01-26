8586b240d08f4d5ff6ef479c1ec491935f73ab80
Shubhankar Das <das.shubhankar97@gmail.com>
2024-01-26 22:48:55 +0530

    finished muxer

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -1,6 +1,10 @@
 import { exec } from "child_process";
 import { Request } from "express";
 import { IResponsePayload } from "types";
+import fs from "fs";
+
+const fsPath = process.env.FS_PATH || ".";
+const ffmpegPath = process.env.FFMPEG_PATH || "";
 
 export function runcmd(command: string): Promise<string> {
   return new Promise((resolve, reject) => {
@@ -13,8 +17,33 @@ export function runcmd(command: string): Promise<string> {
     });
   });
 }
+
 export async function sleep(delay: number) {
   return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+export async function runProcess(payload: { chainCmds?: string[]; output?: string }) {
+  const { chainCmds, output } = payload;
+
+  let cmd = `${ffmpegPath}ffmpeg -y`;
+
+  if (chainCmds && chainCmds.length > 0) {
+    chainCmds.forEach((chainCmd) => {
+      const [key, value] = chainCmd.split(" ");
+      if (key === "-i" || key === "-passlogfile") {
+        cmd += ` ${key} ${fsPath}/${value}`;
+      } else {
+        cmd += ` ${chainCmd}`;
+      }
+    });
+  }
+
+  if (output) {
+    await fs.promises.mkdir(`${fsPath}/${output.split("/").slice(0, -1).join("/")}`, { recursive: true });
+    cmd += ` ${fsPath}/${output}`;
+  }
+
+  return runcmd(cmd);
 }
 
 export function getWebhookResponsePayload(req: Request, status: number, responseTime: number): IResponsePayload {
