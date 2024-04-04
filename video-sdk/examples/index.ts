1d6fc8150f505029b070cb9593304e020e7b5722
Gulam Hussain <gulam@quinn.live>
2024-04-04 15:18:31 +0530

    Bumped

--- video-sdk/examples/index.ts
+++ video-sdk/examples/index.ts
@@ -5,15 +5,7 @@ const lamar = new Lamar({ apiKey: "b8ff590052b699843e0f24fa1eabf8" });
 (async () => {
   // Example 2 - Scale and then trim a video
   try {
-    const video1 = lamar.input({ assetId: "tyuiojhgb67" });
-    const outputAssetId = await lamar.process(video1, {
-      output: "mp4",
-      name: "video1",
-      handle: "video1",
-    });
-    lamar.subscribe(outputAssetId, ({ message, ok, data }) => {
-      console.log(data);
-    });
+    const assets = await lamar.asset.list({ tagId: "60f4b1b1c4b4f4001f1b1b1b" });
   } catch (err) {
     console.log(`err`, err);
   }
