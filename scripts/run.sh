18e0e9d6e38eefd1acdb86daf8125e741542d7ea
Shubhankar Das <das.shubhankar97@gmail.com>
2024-04-05 22:15:46 +0530

    changes in dockerfile

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@ mkdir -p $FS_PATH
 # mount file store if fs ip is set
 if [ -n "$FS_IP" ]; then
   echo "Mounting Cloud Filestore."
-  mount --verbose -o nolock $FS_IP:/$FS_SHARE_NAME $FS_PATH
+  mount -t nfs -o 'vers=3' -v $FS_IP:/$FS_SHARE_NAME $FS_PATH
   echo "Mounting completed."
 fi
 
