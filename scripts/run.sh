333db5af83814fbf392d0b158c340645d71db39c
Shubhankar Das <das.shubhankar97@gmail.com>
2024-12-30 15:27:47 +0530

    changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,9 +7,19 @@ mkdir -p $FS_PATH
 # mount file store if fs ip is set
 if [ -n "$FS_IP" ]; then
   echo "Mounting Cloud Filestore."
-  mount -o nolock -v $FS_IP:/$FS_SHARE_NAME $FS_PATH
+  # if Cloud storage type is S3
+  if [ "$CLOUD_STORAGE_TYPE" = "S3" ]; then
+    mount -t nfs4 -o nfsvers=4.1 $FS_IP:/ $FS_PATH
+  fi
+
+  # if Cloud storage type is GCS
+  if [ "$CLOUD_STORAGE_TYPE" = "GCS" ]; then
+    mount -o nolock -v $FS_IP:/$FS_SHARE_NAME $FS_PATH
+  fi
+
   echo "Mounting completed."
 fi
+
 
 ffmpeg -version
 echo "vmaf version:"
