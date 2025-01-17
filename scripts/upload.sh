061b9ee9f1431e37b67d2967c24de1f670ee768d
Shubhankar Das <das.shubhankar97@gmail.com>
2025-01-17 19:40:51 +0530

    changes

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yarn && yarn build:lambda
-gcloud storage cp dist/*.zip gs://lamar-infra-assets/worker-lambda/$1/
+gcloud storage cp dist/*.zip gs://lamar-infra-assets/worker-lambda/$1
 
 docker build -t quinninc/ffmpeg-worker:$1 .
 docker push quinninc/ffmpeg-worker:$1
