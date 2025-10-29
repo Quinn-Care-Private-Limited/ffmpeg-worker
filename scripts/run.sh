5bae83e345ca8450f6b69fee0435056d66ecbc95
Gulam Hussain <100669151+gulam-quinn@users.noreply.github.com>
2025-10-29 21:39:29 +0530

    changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,14 +2,14 @@
 set -eo pipefail
 
 # Create mount directory for service.
-mkdir -p $FS_PATH
+# mkdir -p $FS_PATH
 
 # mount file store if fs ip is set
-if [ -n "$FS_IP" ]; then
-  echo "Mounting Cloud Filestore."
-  mount -o nolock -v $FS_IP:/$FS_SHARE_NAME $FS_PATH
-  echo "Mounting completed."
-fi
+# if [ -n "$FS_IP" ]; then
+#   echo "Mounting Cloud Filestore."
+#   mount -o nolock -v $FS_IP:/$FS_SHARE_NAME $FS_PATH
+#   echo "Mounting completed."
+# fi
 
 ffmpeg -version
 echo "vmaf version:"
